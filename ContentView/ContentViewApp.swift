5a1276d36f17d21ee77b1923f4183cbfa6495642
OnoueUkyo <u.onoue@kyu-ds.com>
2023-07-25 15:05:28 +0900

    Initial Commit

---
+++ ContentView/ContentViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContentViewApp.swift
+//  ContentView
+//
+//  Created by OnoueUkyo on 2023/07/25.
+//
+
+import SwiftUI
+
+@main
+struct ContentViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
