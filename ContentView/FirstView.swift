bf94682905ce9f114aecdf9cada45269a9e83e47
OnoueUkyo <u.onoue@kyu-ds.com>
2023-07-26 14:44:04 +0900

    first commit

---
+++ ContentView/FirstView.swift
@@ -0,0 +1,20 @@
+//
+//  FirstView.swift
+//  ContentView
+//
+//  Created by OnoueUkyo on 2023/07/26.
+//
+
+import SwiftUI
+
+struct FirstView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FirstView_Previews: PreviewProvider {
+    static var previews: some View {
+        FirstView()
+    }
+}
---
+++ ContentView/ThirdView.swift
@@ -0,0 +1,20 @@
+//
+//  ThirdView.swift
+//  ContentView
+//
+//  Created by OnoueUkyo on 2023/07/26.
+//
+
+import SwiftUI
+
+struct ThirdView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ThirdView_Previews: PreviewProvider {
+    static var previews: some View {
+        ThirdView()
+    }
+}
